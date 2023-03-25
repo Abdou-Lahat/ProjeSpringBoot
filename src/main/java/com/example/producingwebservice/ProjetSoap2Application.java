3f898e1a2f7de2e13aef02511329936867515418
Abdou-Lahat <128923642+Abdou-Lahat@users.noreply.github.com>
2023-03-25 18:22:12 +0100

    Add files via upload

---
+++ src/main/java/com/example/producingwebservice/ProjetSoap2Application.java
@@ -0,0 +1,12 @@
+package com.example.producingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ProjetSoap2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSoap2Application.class, args);
+	}
+
+}
